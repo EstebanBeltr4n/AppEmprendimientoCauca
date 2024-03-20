d751bfc4893e02712d8c1146583789a2716c2153
EstebanBeltr4n <eescarraga@unicauca.edu.co>
2024-03-19 21:55:04 -0500

    Se crea  logo de la aplicacion Emprendimientos del Cauca con exito

---
+++ app/src/main/java/com/example/appemprendimientocauca/StartSesionScreen.kt
@@ -0,0 +1,8 @@
+package com.example.appemprendimientocauca
+
+import android.icu.text.CaseMap.Title
+import androidx.annotation.StringRes
+
+enum class StartSesionScreen (@StringRes val title: Int){
+
+}
